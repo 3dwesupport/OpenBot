c70b7f55719bbc6f2144e1ec071da7855999ba47
Eashika <eashika.singla@itinker.io>
2024-02-05 10:50:55 +0530

    code optimisation

--- services/src/components/common/billingHeader/billingHeader.js
+++ services/src/components/common/billingHeader/billingHeader.js
@@ -1,98 +1,130 @@
-import {Box, Modal} from "@mui/material";
+import {Modal} from "@mui/material";
 import {Images} from "../../../utils/images";
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useReducer} from "react";
 import './billingHeaderModule.css'
 import {Month} from "../../../utils/constants";
 import {getYears} from "../../../database/APIs/projects";
 
-export function BillingHeaderComponent(props) {
-    const {title, onDataChange} = props
-    const currentDate = new Date();
-    const currentMonth = Month[currentDate.getMonth()]
-    const currentYear = currentDate.getFullYear();
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [selectedMonth, setSelectedMonth] = useState('');
-    const [selectedYear, setSelectedYear] = useState('');
-    const [isYearModalOpen, setIsYearModalOpen] = useState(false);
-    const [years, setYears] = useState([new Date().getFullYear()]);
 
-    const handleYearModalOpen = async () => {
-        setIsYearModalOpen(true);
-    };
+let initialState = {
+    selectedModal: null,
+    selectedMonth: '',
+    selectedYear: '',
+    years: [new Date().getFullYear()],
+};
+const actionTypes = {
+    OPEN_MODAL: 'OPEN_MODAL',
+    CLOSE_MODAL: 'CLOSE_MODAL',
+    SELECT_MONTH: 'SELECT_MONTH',
+    SELECT_YEAR: 'SELECT_YEAR',
+    SET_YEARS: 'SET_YEARS',
+};
 
-    const handleYearModalClose = () => {
-        setIsYearModalOpen(false);
-    };
-    const handleModalOpen = () => {
-        setIsModalOpen(true);
-    };
 
-    const handleModalClose = () => {
-        setIsModalOpen(false);
-    };
-
-    const handleMonthClick = (month) => {
-        setSelectedMonth(month);
-        onDataChange(month)
-        setIsModalOpen(false);
-    };
-    const handleYearClick = (year) => {
-        setSelectedYear(year);
-        onDataChange(year)
-        setIsYearModalOpen(false);
+const reducer = (state, action) => {
+    switch (action.type) {
+        case actionTypes.OPEN_MODAL:
+            return {...state, selectedModal: action.payload};
+        case actionTypes.CLOSE_MODAL:
+            return {...state, selectedModal: null};
+        case actionTypes.SELECT_MONTH:
+            return {...state, selectedMonth: action.payload};
+        case actionTypes.SELECT_YEAR:
+            return {...state, selectedYear: action.payload};
+        case actionTypes.SET_YEARS:
+            return {...state, years: action.payload};
+        default:
+            throw new Error('Unknown action type: ' + action.type);
     }
+};
+
+
+export function BillingHeaderComponent(props) {
+    const {
+        title,
+        onDataChange
+    } = props
+    const [state, dispatch] = useReducer(reducer, initialState);
+    const {selectedModal, selectedMonth, selectedYear, years} = state;
+
+    const handleModalAction = (actionType, payload) => {
+        console.log('Action Type:::::::', actionType);
+        console.log('Payload::::::', payload);
+        console.log('Before dispatch - Selected Modal:', state.selectedModal);
+        switch (actionType) {
+            case actionTypes.OPEN_MODAL:
+                dispatch({type: actionTypes.OPEN_MODAL, payload});
+                break;
+            case actionTypes.CLOSE_MODAL:
+                dispatch({type: actionTypes.CLOSE_MODAL});
+                break;
+            case actionTypes.SELECT_MONTH:
+                dispatch({type: actionTypes.SELECT_MONTH, payload});
+                onDataChange(payload);
+                dispatch({type:actionTypes.CLOSE_MODAL})
+                break;
+            case actionTypes.SELECT_YEAR:
+                dispatch({type: actionTypes.SELECT_YEAR, payload});
+                onDataChange(payload);
+                dispatch({type:actionTypes.CLOSE_MODAL})
+                break;
+            default:
+                throw new Error('Unknown action type: ' + actionType);
+        }
+        console.log('After dispatch - Selected Modal:', state.selectedModal);
+    };
 
     useEffect(() => {
         getYears().then((res) => {
-            if (res?.length === 0) return
-            setYears(res);
-        })
+            if (res?.length === 0) return;
+            dispatch({type: actionTypes.SET_YEARS, payload: res});
+        });
     }, []);
+
 
     return (
         <>
-            {isYearModalOpen &&
-                <Modal open={isYearModalOpen} onClose={handleYearModalClose}>
-                        <div className={"modalYear"}>
-                            {years.map((year, index) => (
-                                <div onClick={() => handleYearClick(year)} key={index} style={{padding: '10px'}}
-                                     className={"items"}>
-                                    {year}
-                                </div>
-                            ))}
-                        </div>
-                </Modal>
-            }
-            {
-                isModalOpen &&
-                <Modal open={isModalOpen} onClose={handleModalClose}>
+            {selectedModal === 'year' && (
+                <Modal open={true} onClose={() => handleModalAction(actionTypes.CLOSE_MODAL)}>
+                    <div className={"modalYear"}>
+                        {years.map((year, index) => (
+                            <div className={"items"} key={index}
+                                 onClick={() => handleModalAction(actionTypes.SELECT_YEAR, year)}>
+                                {year}
+                            </div>
+                        ))}
+                    </div>
+                </Modal>)}
+            {selectedModal === 'month' && (
+                <Modal open={true} onClose={() => handleModalAction(actionTypes.CLOSE_MODAL)}>
                     <div className={"modalMonth"}>
                         {Month.map((month, index) => (
-                            <div onClick={() => handleMonthClick(month)} style={{padding: "10px"}} className={"items"}
-                                 key={index}>{month}</div>
+                            <div className={"items"} key={index}
+                                 onClick={() => handleModalAction(actionTypes.SELECT_MONTH, month)}>
+                                {month}
+                            </div>
                         ))}
                     </div>
                 </Modal>
+            )
             }
-                <div className={"billingHeaderDiv"}>
-                    <div className={"billingHeaderTextDiv"}>
-                        <span className={"billingHeaderText"}>{title}</span>
-                        <div className={"calenderDiv"}>
-                            <div className={"yearDiv"} onClick={handleYearModalOpen}>
-                                {selectedYear ? selectedYear : (
-                                    <span className={"yearText"}>{currentYear}</span>)}
-                                <img className={"billingHeaderImage"} src={Images.billingHeader_icon}
-                                     alt={"icon"}></img>
-                            </div>
-                            <div onClick={handleModalOpen} className={"modalDiv"}>
-                                {selectedMonth ? (<span>{selectedMonth}</span>) : (
-                                    <span className={"monthText"}>{currentMonth}</span>)}
-                                <img className={"billingHeaderImage"} src={Images.billingHeader_icon}
-                                     alt={"icon"}></img>
-                            </div>
+            <div className={"billingHeaderDiv"}>
+                <div className={"billingHeaderTextDiv"}>
+                    <span className={"billingHeaderText"}>{title}</span>
+                    <div className={"calenderDiv"}>
+                        <div className={"yearDiv"} onClick={() => handleModalAction(actionTypes.OPEN_MODAL, 'year')}>
+                            {selectedYear ? selectedYear : (
+                                <span className={"yearText"}>{new Date().getFullYear()}</span>)}
+                            <img className={"billingHeaderImage"} src={Images.billingHeader_icon} alt={"icon"}/>
+                        </div>
+                        <div className={"modalDiv"} onClick={() => handleModalAction(actionTypes.OPEN_MODAL, 'month')}>
+                            {selectedMonth ? (<span>{selectedMonth}</span>) : (
+                                <span className={"monthText"}>{Month[new Date().getMonth()]}</span>)}
+                            <img className={"billingHeaderImage"} src={Images.billingHeader_icon} alt={"icon"}/>
                         </div>
                     </div>
                 </div>
+            </div>
         </>
     )
 }
--- services/src/pages/analytics/usageAnalysisCard.js
+++ services/src/pages/analytics/usageAnalysisCard.js
@@ -2,6 +2,7 @@ import Card from "@mui/material/Card";
 import React from "react";
 import './usageAnalysisCard.css'
 import {UserAnalysisCardData} from "../../utils/constants";
+import {rgba} from "@react-spring/shared";
 
 /**
  * function to display cards on the usageAnalysis Page
@@ -13,7 +14,7 @@ export function UsageAnalysisCardComponent(props) {
         <>
             <div className={"analysisCardContainer"}>
                 {UserAnalysisCardData.map((card, index) => (
-                    <Card sx={{borderRadius: 2}} key={index}>
+                    <Card sx={{borderRadius: 2, boxShadow: 5}} key={index}>
                         <span className={"analysisCardText"}> {card.text}</span>
                         <div className={"cardData"}>
                             {index === 0 ? props.usageDetails.projects : index === 1 ? props.usageDetails.models : index === 2 ? 0 : index === 3 ? props.usageDetails.server : 0}
