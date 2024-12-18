5730ab67c0c1720348674c6f0bd8356873a98f3b
isha382 <isha.chaudhary@itinker.io>
2024-12-18 17:21:17 +0530

    changed dialog box to message toast for mode selection

--- controller/flutter/lib/screens/settingsDrawer.dart
+++ controller/flutter/lib/screens/settingsDrawer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:nsd/nsd.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:openbot_controller/globals.dart';
 import 'package:toggle_switch/toggle_switch.dart';
 
@@ -23,6 +24,7 @@ class _SettingsDrawerState extends State<SettingsDrawer> {
   late List<DropdownMenuItem<String>> items = [];
   bool isNoise = false;
   bool isNetwork = false;
+  int _currentIndex = 0; // Maintain the current index
 
   // Function to generate DropdownMenuItem widgets
   List<DropdownMenuItem<String>> buildDropdownMenuItems() {
@@ -73,131 +75,85 @@ class _SettingsDrawerState extends State<SettingsDrawer> {
     return items;
   }
 
-  // Function to show the dialog
-  void _showModeDialog(String title, String message) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20), // Rounded corners
-          ),
-          backgroundColor: Colors.white, // Change to fit your theme
-          title: Row(
-            children: [
-              Icon(
-                Icons.info_outline, // Choose an appropriate icon
-                color: Colors.blue,
-              ),
-              SizedBox(width: 10),
-              Text(
-                title,
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                ),
-              ),
-            ],
-          ),
-          content: Text(
-            message,
-            style: TextStyle(fontSize: 16, color: Colors.grey[700]),
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop(); // Close the dialog
-              },
-              style: TextButton.styleFrom(
-                backgroundColor: Colors.blue,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-              child: Text(
-                'OK',
-                style: TextStyle(color: Colors.white),
-              ),
-            ),
-          ],
-        );
-      },
+  // Function to show a toast message
+  void _showToast(String message) {
+    Fluttertoast.showToast(
+      msg: message,
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+      backgroundColor: Colors.white,
+      textColor: Colors.black,
+      fontSize: 14.0,
     );
   }
-
-
-
-  int _currentIndex = 0; // Maintain the current index
-
 
   @override
   Widget build(BuildContext context) {
     return Drawer(
-        backgroundColor: const Color(0xFF202020),
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            Padding(
-                padding: const EdgeInsets.only(left: 30, top: 35),
-                child: Row(children: [
-                  const Text(
-                    'Controller',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w500,
-                      color: Color(0xFF0071C5),
-                    ),
+      backgroundColor: const Color(0xFF202020),
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 30, top: 35),
+            child: Row(
+              children: [
+                const Text(
+                  'Controller',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                    color: Color(0xFF0071C5),
                   ),
-                  const SizedBox(
-                    width: 30,
-                  ),
-                  ToggleButtons(
-                    borderRadius: BorderRadius.circular(10),
-                    selectedBorderColor:
-                        const Color(0xFF0071C5).withOpacity(0.4),
-                    disabledBorderColor:
-                        const Color(0xFF0071C5).withOpacity(0.4),
-                    onPressed: (int newIndex) {
-                      setState(() {
-                        for (int index = 0;
-                            index < isSelected.length;
-                            index++) {
-                          if (index == newIndex) {
-                            isSelected[index] = true;
-                            if (index == 0) {
-                              setState(() {
-                                widget.onSettingsChanged(true, false);
-                              });
-                            } else {
-                              setState(() {
-                                widget.onSettingsChanged(false, true);
-                              });
-                            }
+                ),
+                const SizedBox(
+                  width: 30,
+                ),
+                ToggleButtons(
+                  borderRadius: BorderRadius.circular(10),
+                  selectedBorderColor: const Color(0xFF0071C5).withOpacity(0.4),
+                  disabledBorderColor: const Color(0xFF0071C5).withOpacity(0.4),
+                  onPressed: (int newIndex) {
+                    setState(() {
+                      for (int index = 0; index < isSelected.length; index++) {
+                        if (index == newIndex) {
+                          isSelected[index] = true;
+                          if (index == 0) {
+                            setState(() {
+                              widget.onSettingsChanged(true, false);
+                            });
                           } else {
-                            isSelected[index] = false;
+                            setState(() {
+                              widget.onSettingsChanged(false, true);
+                            });
                           }
+                        } else {
+                          isSelected[index] = false;
                         }
-                      });
-                    },
-                    isSelected: isSelected,
-                    children: [
-                      Image.asset(
-                        "images/tilting_phone_icon.png",
-                        height: 33,
-                        width: 33,
-                      ),
-                      Image.asset(
-                        "images/controller_icon.png",
-                        height: 33,
-                        width: 33,
-                      )
-                    ],
-                  ),
-                ])),
-            Padding(
-              padding: const EdgeInsets.only(left: 30, top: 35),
-              child: Row(children: [
+                      }
+                    });
+                  },
+                  isSelected: isSelected,
+                  children: [
+                    Image.asset(
+                      "images/tilting_phone_icon.png",
+                      height: 33,
+                      width: 33,
+                    ),
+                    Image.asset(
+                      "images/controller_icon.png",
+                      height: 33,
+                      width: 33,
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 30, top: 35),
+            child: Row(
+              children: [
                 const Text(
                   'Server',
                   style: TextStyle(
@@ -206,9 +162,7 @@ class _SettingsDrawerState extends State<SettingsDrawer> {
                     color: Color(0xFF0071C5),
                   ),
                 ),
-                const SizedBox(
-                  width: 54,
-                ),
+                const SizedBox(width: 54),
                 DropdownButton(
                   value: dropDownValue,
                   borderRadius: const BorderRadius.all(Radius.circular(3)),
@@ -231,11 +185,13 @@ class _SettingsDrawerState extends State<SettingsDrawer> {
                     }
                   },
                 ),
-              ]),
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.only(left: 30, top: 35),
-              child: Row(children: [
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 30, top: 35),
+            child: Row(
+              children: [
                 const Text(
                   'Noise',
                   style: TextStyle(
@@ -244,9 +200,7 @@ class _SettingsDrawerState extends State<SettingsDrawer> {
                     color: Color(0xFF0071C5),
                   ),
                 ),
-                const SizedBox(
-                  width: 54,
-                ),
+                const SizedBox(width: 54),
                 Switch(
                   value: isNoise,
                   onChanged: (bool value) {
@@ -259,60 +213,60 @@ class _SettingsDrawerState extends State<SettingsDrawer> {
                   },
                   activeColor: const Color(0xFF0071C5),
                 )
-              ]),
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.only(left: 30, top: 35),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  const Text(
-                    'Mode',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w500,
-                      color: Color(0xFF0071C5),
-                    ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 30, top: 35),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const Text(
+                  'Mode',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                    color: Color(0xFF0071C5),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 50.0),
-                    child: ToggleSwitch(
-                      minWidth: 80.0,
-                      cornerRadius: 20.0,
-                      initialLabelIndex: _currentIndex, // Use the state to set the initial index
-                      activeFgColor: Colors.white,
-                      inactiveBgColor: Colors.grey,
-                      inactiveFgColor: Colors.white,
-                      totalSwitches: 2,
-                      labels: ['Manual', 'VAD'],
-                      activeBgColors: [
-                        [const Color(0xFF0071C5)],
-                        [const Color(0xFF0071C5)],
-                      ],
-                      onToggle: (index) {
-                        // Prevent toggling on the same item
-                        if (index != _currentIndex) {
-                          setState(() {
-                            _currentIndex = index!; // Update the current index
-                          });
-                          widget.onVADModeChanged();
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 50.0),
+                  child: ToggleSwitch(
+                    minWidth: 80.0,
+                    cornerRadius: 20.0,
+                    initialLabelIndex: _currentIndex,
+                    activeFgColor: Colors.white,
+                    inactiveBgColor: Colors.grey,
+                    inactiveFgColor: Colors.white,
+                    totalSwitches: 2,
+                    labels: ['Manual', 'VAD'],
+                    activeBgColors: [
+                      [const Color(0xFF0071C5)],
+                      [const Color(0xFF0071C5)],
+                    ],
+                    onToggle: (index) {
+                      if (index != _currentIndex) {
+                        setState(() {
+                          _currentIndex = index!;
+                        });
+                        widget.onVADModeChanged();
 
-                          // Show a popup with the selected mode message
-                          if (index == 0) {
-                            // Manual mode selected
-                            _showModeDialog('Manual Mode Activated', 'Now press the mic.');
-                          } else {
-                            // VAD mode selected
-                            _showModeDialog('VAD Mode Activated', 'Start speaking.');
-                          }
+                        // Show a toast message
+                        if (index == 0) {
+                          _showToast(
+                              'Manual Mode Activated. Now press the mic.');
+                        } else {
+                          _showToast('VAD Mode Activated. Start speaking.');
                         }
-                      },
-                    ),
+                      }
+                    },
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
   }
 }
