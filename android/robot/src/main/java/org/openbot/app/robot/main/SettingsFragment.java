6d3152f51a6c3db98ff9f1e601a887193923e32a
Harish Thakur <harish.thakur@itinker.io>
2026-09-15 16:42:00 +0530

    Fix storage permission settings redirect

--- android/robot/src/main/java/org/openbot/app/robot/main/SettingsFragment.java
+++ android/robot/src/main/java/org/openbot/app/robot/main/SettingsFragment.java
@@ -5,6 +5,7 @@ import static org.openbot.app.robot.utils.Constants.PERMISSION_BLUETOOTH_CONNECT
 import static org.openbot.app.robot.utils.Constants.PERMISSION_BLUETOOTH_SCAN;
 import static org.openbot.app.robot.utils.Constants.PERMISSION_CAMERA;
 import static org.openbot.app.robot.utils.Constants.PERMISSION_LOCATION;
+import static org.openbot.app.robot.utils.Constants.PERMISSION_STORAGE;
 
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -53,6 +54,13 @@ public class SettingsFragment extends PreferenceFragmentCompat {
                         else {
                           location.setChecked(false);
                           PermissionUtils.showLocationPermissionSettingsToast(requireActivity());
+                        }
+                        break;
+                      case PERMISSION_STORAGE:
+                        if (granted) storage.setChecked(true);
+                        else {
+                          storage.setChecked(false);
+                          PermissionUtils.showStoragePermissionSettingsToast(requireActivity());
                         }
                         break;
                       case PERMISSION_AUDIO:
@@ -111,8 +119,11 @@ public class SettingsFragment extends PreferenceFragmentCompat {
               PermissionUtils.requestStoragePermission(requireActivity());
             } else if (storage.isChecked()) {
               PermissionUtils.startInstalledAppDetailsActivity(requireActivity());
+            } else if (PermissionUtils.shouldAskForPermission(requireActivity(), PERMISSION_STORAGE)) {
+              PermissionUtils.markedPermissionAsAsked(requireActivity(), PERMISSION_STORAGE);
+              requestPermissionLauncher.launch(new String[] {PERMISSION_STORAGE});
             } else {
-              PermissionUtils.requestStoragePermission(requireActivity());
+              PermissionUtils.startInstalledAppDetailsActivity(requireActivity());
             }
 
             return false;
